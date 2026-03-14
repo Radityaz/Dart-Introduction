951f16f3a7badcb123d138d90998dd210412d3d7
Radityaz <rikoadityazaki@gmail.com>
2026-03-14 09:06:51 +0700

    small change

--- Week1/Practice01.dart
+++ Week1/Practice01.dart
@@ -1,12 +1,11 @@
 void main() {
-  // 1 & 2. Tentukan variabel dan isi dengan data [cite: 22, 24]
   String nama = "Electra Seafood"; 
   int tahunDidirikan = 2023; 
   String pemilik = "Mbok Yem"; 
   String alamat = "JL. Prof. Soedarto, SH, Tembalang"; 
-  bool statusBuka = true; // Buka = true [cite: 11]
+  bool statusBuka = true; // Buka = true 
 
-  // Daftar makanan menggunakan List of Maps [cite: 26, 29]
+  // Daftar makanan 
   List daftarMakanan = [
     {'nama': 'Kepiting Rebus', 'harga': 40000}, 
     {'nama': 'Nasi Goreng', 'harga': 20000}, 
@@ -14,13 +13,12 @@ void main() {
     {'nama': 'Sate Cumi', 'harga': 30000}, 
   ];
 
-  // Daftar minuman menggunakan List of Maps [cite: 26, 29]
+  // Daftar minuman 
   List daftarMinuman = [
     {'nama': 'Es Jeruk', 'harga': 5000}, 
     {'nama': 'Es Teh', 'harga': 2000}, 
     {'nama': 'Es Jus', 'harga': 6000}, 
   ];
 
-  // 3. Cetak data restoran [cite: 25]
   print('nama: $nama, tahun: $tahunDidirikan, pemilik: $pemilik, alamat: $alamat, status: $statusBuka, daftarMakanan: $daftarMakanan, daftarMinuman: $daftarMinuman');
 }
